3c818de9a3ae00e848bb2b9f9b17a1f1ef6b12f5
Kainat zia <154695099+Kainat-zia@users.noreply.github.com>
2024-05-01 15:49:45 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+alert("thankyou for coming")
+// console.log("careful writer ends every declarative sentence with a aperiod")
+
+var first_name = "kainat";
+console.log(first_name)
+let firstName = "zia";
+console.log(firstName)
+
+var first_name; // declair
+first_name = "kainat";
+var first_name = "kainat";
+
+var number = 2021;
+var name = "kainat zia"
+var dateOfBirth = 2002;
+var email = "kainatzia@gmail.com"
+console.log(name + "   " + dateOfBirth + "    " + email)
